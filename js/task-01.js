4a5c6a7db3a51a4d9253922fa700c8da88ddf94f
TonyShimm <101990825+TonyShimm@users.noreply.github.com>
2023-08-09 16:46:39 -0400

    Update task-01.js

--- js/task-01.js
+++ js/task-01.js
@@ -1,2 +1,9 @@
-const itemEl = document.querySelectorAll(".item");
-console.log(itemEl);
+const categoriesAll = document.querySelector("#categories");
+const categoriesLength = categoriesAll.children.length;
+console.log(`Number of categories: ${categoriesLength}`);
+
+const itemsElement = document.querySelectorAll(".item");
+itemsElement.forEach((item) => {
+  console.log("Category: ", item.firstElementChild.textContent);
+  console.log("Elements: ", item.lastElementChild.children.length);
+});
